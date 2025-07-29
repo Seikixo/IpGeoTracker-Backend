e26c8f986ec2165d1fe4caca6c5fd57f74af0332
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-07-30 01:07:59 +0800

    feat: update authentication and CORS configuration

--- jlabsbackend/routes/api.php
+++ jlabsbackend/routes/api.php
@@ -6,7 +6,12 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest')
